935937416e865db8974e716d32242cb6d070ac26
AndresFMoya <andresfmoya@gmail.com>
2019-10-19 12:21:27 -0500

    Add partials layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -25,9 +25,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'title', "About | #{@base_title}"
   end
 
-  test 'should get contact' do
+  test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select 'title', "Contact | #{@base_title}"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end

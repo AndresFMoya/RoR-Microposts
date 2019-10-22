2dc15b8544daacfdc7e47f6f5ca8255950a92b54
AndresFMoya <andresfmoya@gmail.com>
2019-10-22 15:11:38 -0500

    Generate Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'

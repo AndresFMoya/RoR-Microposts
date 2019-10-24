658a442d3b63b9f61d86f795a9b843c27f7d0e90
AndresFMoya <andresfmoya@gmail.com>
2019-10-24 11:22:53 -0500

    Add log in upon sign up

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid information" do
     get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,5 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
+
   end
 end

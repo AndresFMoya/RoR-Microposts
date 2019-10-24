b44ac34edb8b4c69af97d46e55cdbb80390ee42f
AndresFMoya <andresfmoya@gmail.com>
2019-10-24 17:05:58 -0500

    Deploy to Heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-require ('bootstrap')
+require("jquery")
+import "bootstrap"
 
 
 

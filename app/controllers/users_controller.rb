f222cd81226c85630b4e0e274dcfaa0fc6a1486c
AndresFMoya <andresfmoya@gmail.com>
2019-10-28 09:57:58 -0500

    Add edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
-  
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+      
     # Returns the hash digest of the given string.
     def User.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :

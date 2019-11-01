fcf312721fffb3743adda640911e37c51f6d2268
AndresFMoya <andresfmoya@gmail.com>
2019-11-01 16:35:40 -0500

    Test password reset mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token, :reset_token  before_save   :downcase_email
+  attr_accessor :remember_token, :activation_token, :reset_token  
+  before_save   :downcase_email
   before_create :create_activation_digest
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i

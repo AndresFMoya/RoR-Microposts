2a05ffd893dff07a391a7457db2fc9a677bc02f0
AndresFMoya <andresfmoya@gmail.com>
2019-11-01 16:20:09 -0500

    Update password reset mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
-  before_save   :downcase_email
+  attr_accessor :remember_token, :activation_token, :reset_token  before_save   :downcase_email
   before_create :create_activation_digest
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -49,6 +48,18 @@ end
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private

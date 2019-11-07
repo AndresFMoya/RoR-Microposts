ad7c605799c3d73382143c7bc804b0fb09906f24
AndresFMoya <andresfmoya@gmail.com>
2019-11-07 17:22:59 -0500

    Add users/microposts associations

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content:"Lorem Ipsum", user_id: @user.id)
+    @micropost = @user.microposts.new(content:"Lorem Ipsum")
   end
 
   test "should_be_valid" do
@@ -12,6 +12,16 @@ class MicropostTest < ActiveSupport::TestCase
 
   test "user id should be present" do
     @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
 

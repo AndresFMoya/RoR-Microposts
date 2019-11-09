2117aaad27b77d3b8bbf7e01e7273b0e3cf92aea
AndresFMoya <andresfmoya@gmail.com>
2019-11-09 08:45:56 -0500

    fix: Fix Micropost(s) controller name

--- app/controllers/micropost_controller.rb
+++
@@ -1,2 +0,0 @@
-class MicropostController < ApplicationController
-end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MicropostControllerTest < ActionDispatch::IntegrationTest
+class MicropostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end

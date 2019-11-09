3af43f3c6a60fc9b8dfa59f6d27d9d48da4cdb23
AndresFMoya <andresfmoya@gmail.com>
2019-11-09 08:05:48 -0500

    feat: Ordering micropost with descending_order

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  default_scope -> { order(created_at: :desc) }
 end

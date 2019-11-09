60f3985690a087f3769a1bec2012c16749e24722
AndresFMoya <andresfmoya@gmail.com>
2019-11-09 09:10:03 -0500

    feat: render microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
-  end  
+  end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new

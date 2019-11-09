607536d3b8bcedb6cc249da06a94e4522db05569
AndresFMoya <andresfmoya@gmail.com>
2019-11-09 09:21:06 -0500

    test: Add microposts seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,11 @@ User.create!(name:  "Example User",
               password_confirmation: password,
               activated: true,
               activated_at: Time.zone.now)
+
+  # Generate microposts for a subset of users.
+  users = User.order(:created_at).take(6)
+  50.times do
+    content = Faker::Lorem.sentence(word_count: 5)
+    users.each { |user| user.microposts.create!(content: content) }
+  end
 end

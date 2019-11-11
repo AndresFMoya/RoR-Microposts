ee929f5dcd0907adb5667de4288b89bdd0bdacad
AndresFMoya <andresfmoya@gmail.com>
2019-11-11 08:12:33 -0500

    refactor: Move logged_in_user method from Users controller to Application Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,14 +56,6 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])

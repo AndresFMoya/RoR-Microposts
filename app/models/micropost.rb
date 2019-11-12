132ca291d961802461cf3a1ab4dc6aecbed3956c
AndresFMoya <andresfmoya@gmail.com>
2019-11-12 07:59:41 -0500

    feat: add image validation to microposts without script

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,8 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "must be a valid image format" },
+            size:         { less_than: 5.megabytes,
+                            message:   "should be less than 5MB" }
 end

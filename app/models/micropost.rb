e82e6e78aef86517bbab92a7039e4e6ea35f2ed0
AndresFMoya <andresfmoya@gmail.com>
2019-11-12 08:04:46 -0500

    feat: add display_image method to resize uploaded images

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,4 +8,9 @@ class Micropost < ApplicationRecord
                                       message: "must be a valid image format" },
             size:         { less_than: 5.megabytes,
                             message:   "should be less than 5MB" }
+
+  # Returns a resized image for display.
+  def display_image
+    image.variant(resize_to_limit: [200, 200])
+  end
 end

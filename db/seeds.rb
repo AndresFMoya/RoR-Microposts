1c3898f3258827193c2667538f4d220ced1a3e92
AndresFMoya <andresfmoya@gmail.com>
2019-11-12 16:36:32 -0500

    chore: add followers to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,24 @@ User.create!(name:  "Example User",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
-
-  # Generate microposts for a subset of users.
-  users = User.order(:created_at).take(6)
-  50.times do
-    content = Faker::Lorem.sentence(word_count: 5)
-    users.each { |user| user.microposts.create!(content: content) }
-  end
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
+
+# Generate microposts for a subset of users.
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# Create following relationships.
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }

7d19bf0d4f97d97c122a3710f6e07ef37d8a9d2f
AndresFMoya <andresfmoya@gmail.com>
2019-11-12 14:04:44 -0500

    test: add test for following methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
            foreign_key: "follower_id",
            dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -76,6 +77,22 @@ end
   def feed
     Micropost.where('user_id = ?', id)
   end
+
+    # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+    # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+    # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
   private
   # Converts email to all lower-case.
